f0edfa2b3bc1e29bef2633b0ae9286744c75e391
Pierre Guillaume <pierre@tricy.io>
2017-10-27 12:00:00 +0200

    adding CORS

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ const CRASH_EVERY_X_CALLS = 0;
 export default (data) => {
   const server = express();
 
+  server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
+  })
+
   server.use(cookieParser());
 
   server.get('/', (req, res) => {
